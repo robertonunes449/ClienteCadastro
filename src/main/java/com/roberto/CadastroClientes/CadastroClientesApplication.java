ef43bc458938ede58943c1948e94f7f6e9ebcc11
robertonunes449 <roberto.nunes449@gmail.com>
2022-07-06 21:20:07 -0300

    commit inicial

---
+++ src/main/java/com/roberto/CadastroClientes/CadastroClientesApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.CadastroClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roberto/CadastroClientes/CadastroClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.CadastroClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
